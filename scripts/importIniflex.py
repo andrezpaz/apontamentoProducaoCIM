b16c69832b841bb352a80b9c34cded0cf24fc3be
andrez <andrez@htmind.com.br>
2023-03-17 17:14:09 -0300

    Adicionado Peso e Quantidade na FIla de prod

--- scripts/importIniflex.py
+++ scripts/importIniflex.py
@@ -33,14 +33,14 @@ def remove_table():
             cursor.close()
             connection.close()
             print("MySQL connection is closed")
-def insert_varibles_into_table(recurso, etapa, seq_fila, op, cod_item, desc_item, cod_clicheria, iniprog, fimprog, mrp):
+def insert_varibles_into_table(recurso, etapa, seq_fila, op, cod_item, desc_item, cod_clicheria, iniprog, fimprog, mrp, quantidade, peso):
     try:
         connection = create_server_connection()
         cursor = connection.cursor()
-        mySql_insert_query = """INSERT INTO pcpfila (recurso, etapa, seq_fila, op, codigo_item, descricao_item, cod_clicheria, inicioprog, fimprog, mrp) 
+        mySql_insert_query = """INSERT INTO pcpfila (recurso, etapa, seq_fila, op, codigo_item, descricao_item, cod_clicheria, inicioprog, fimprog, mrp, quantidade, peso) 
                                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) """
 
-        record = (recurso, etapa, seq_fila, op, cod_item, desc_item, cod_clicheria, iniprog, fimprog, mrp)
+        record = (recurso, etapa, seq_fila, op, cod_item, desc_item, cod_clicheria, iniprog, fimprog, mrp, quantidade, peso)
         print(record)
         cursor.execute(mySql_insert_query, record)
         connection.commit()
@@ -59,16 +59,17 @@ def insert_varibles_into_table(recurso, etapa, seq_fila, op, cod_item, desc_item
 remove_table() # Remove dados atuais da tablea, já que os novos virão da importação
 
 ### Inicia leitura do arquivo em CSV
-with open ('./import/CIMINIFLEX.csv', newline='', encoding='latin-1') as csvfile:
+with open ('./CIMINIFLEX.csv', newline='', encoding='latin-1') as csvfile:
     fila = csv.reader(csvfile,delimiter=';', quotechar='"')
     for row in fila:
-        #print(', '.join(row))
         recurso = row[2]
         etapa = row[1]
         seq_fila = row[3]
         op = row[4]
         cod_item = row[5]
         desc_item = row[7]
+        quantidade = round(float(row[8].replace(',','.')),3)
+        peso = round(float(row[9].replace(',','.')),3)
         ini_prog = row[10]
         fim_prog = row[11]
         cod_clicheria = row[12]
@@ -77,7 +78,7 @@ with open ('./import/CIMINIFLEX.csv', newline='', encoding='latin-1') as csvfile
                                    int(ini_prog.split("/")[0]), #Dia
                                    int(ini_prog.split(" ")[1].split(":")[0]), ##Hora 
                                    int(ini_prog.split(" ")[1].split(":")[1])).isoformat().replace("T", " ") # Minuto e transformacao
-        if (row[11] is ''):
+        if (row[11] == ''):
             fim_prog_format = ini_prog_format
         else:
             fim_prog_format = datetime(int(fim_prog.split("/")[2].split(" ")[0]),
@@ -86,10 +87,10 @@ with open ('./import/CIMINIFLEX.csv', newline='', encoding='latin-1') as csvfile
                                     int(fim_prog.split(" ")[1].split(":")[0]), 
                                     int(fim_prog.split(" ")[1].split(":")[1])).isoformat().replace("T", " ")
             
-        if (row[13] is ''):
+        if (row[13] == ''):
             numeroMRP = None
         else:
             numeroMRP = row[13]
-        print (numeroMRP)
+    
         insert_varibles_into_table(recurso, etapa, seq_fila, op, cod_item, desc_item, cod_clicheria, 
-                                   ini_prog_format, fim_prog_format, numeroMRP) ## Inicia insert no banco, de forma individual
+                                   ini_prog_format, fim_prog_format, numeroMRP, quantidade, peso) ## Inicia insert no banco, de forma individual
